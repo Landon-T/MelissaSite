7f93c93ee559f98013f6bd20932c3f827396491f
Landon <landon@pop-os.localdomain>
2020-08-10 14:09:17 -0600

    Added meet page and some place holder cards

--- melissa/mel_site/views.py
+++ melissa/mel_site/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mel_site/index.html')
+
+
+def meet(request):
+    return render(request, 'mel_site/meet_mel.html')
