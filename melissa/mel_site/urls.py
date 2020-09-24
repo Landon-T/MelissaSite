d4940720af91e82e53917f8b9ac5237ed52f51fa
Landon <landon@pop-os.localdomain>
2020-09-24 16:17:00 -0600

    Added CMS functionality with markdown

--- melissa/mel_site/urls.py
+++ melissa/mel_site/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    #path("register", views.register, name="register"),
     path("meet", views.meet, name="meet"),
     path("speak", views.speak, name="speak"),
     path("involved", views.involved, name="involved"),
     path("store", views.store, name="store"),
-    path("create",views.create, name="create")
+    path("create",views.create, name="create"),
+    path("edit/<str:title>",views.edit, name="edit")
+
 ]
--- melissa/mel_site/views.py
+++ melissa/mel_site/views.py
@@ -1,15 +1,20 @@
+import json
+import markdown2
+import random
 from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-import markdown2
-import random
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+
 from . import util
 
 
 class NewPostForm(forms.Form):
     title = forms.CharField(label="Title")
-    body = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":50}))
+    body = forms.CharField(widget=forms.Textarea(attrs={"rows":25, "cols":100}))
 
 # Create your views here.
 def index(request):
@@ -23,7 +28,7 @@ def speak(request):
     entry = util.get_entry("Speaking & Events")
     if entry != None:
         return render(request, 'mel_site/speak_events.html', {
-            "content": markdown2.markdown(entry), 
+            "content": markdown2.markdown(entry, extras=["footnotes"]), 
             "title": "Speaking & Events"
         })
     return render(request, 'mel_site/speak_events.html')
@@ -32,7 +37,7 @@ def involved(request):
     entry = util.get_entry("Get Involved")
     if entry != None:
         return render(request, 'mel_site/get_involved.html', {
-            "content": markdown2.markdown(entry), 
+            "content": markdown2.markdown(entry, extras=["footnotes"]), 
             "title": "Get Involved"
         })
     return render(request, 'mel_site/get_involved.html')
@@ -40,26 +45,97 @@ def involved(request):
 def store(request):
     return render(request, 'mel_site/store.html')
 
-def create(request):
 
-    if request.method == "POST":
-        form = NewPostForm(request.POST)
-        if form.is_valid():
-            #Save the article
-            util.save_entry(form.cleaned_data["title"], form.cleaned_data["body"])
-            #return HttpResponseRedirect(reverse("wiki", args=[form.cleaned_data["title"]]))
-            #return somthing new here
-        else:
-            # return the filled form if there is an error
-            return render(request, "mel_site/create.html",{
-                "form": form
-            })
-    return render(request, "mel_site/create.html",{
-        "form": NewPostForm()
-    })
+
+def create(request):
+    if request.user.is_authenticated:
+    # Do something for authenticated users.
+        pages = util.list_entries
+        if request.method == "POST":
+            form = NewPostForm(request.POST)
+            if form.is_valid():
+                #Save the article
+                util.save_entry(form.cleaned_data["title"], form.cleaned_data["body"])
+                #return HttpResponseRedirect(reverse("wiki", args=[form.cleaned_data["title"]]))
+                #return somthing new here
+            else:
+                # return the filled form if there is an error
+                return render(request, "mel_site/create.html",{
+                    "form": form,
+                    "pages": pages,
+                    "title": None
+                })
+        return render(request, "mel_site/create.html",{
+            "form": NewPostForm(),
+            "pages": pages,
+            "title": None
+        })
+    
+    else:
+        #ask user to login
+        return render(request, "mel_site/login.html")
+
 
 def edit(request, title):
-    form = NewArticleForm({'title':title, 'body': util.get_entry(title) })
+    pages = util.list_entries
+    form = NewPostForm({'title':title, 'body': util.get_entry(title) })
     return render(request, "mel_site/create.html",{ 
-        "form":form
+        "form":form,
+        "pages": pages,
+        "title": title
     })
+
+
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "mel_site/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "mel_site/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "mel_site/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "mel_site/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "mel_site/register.html")
+
