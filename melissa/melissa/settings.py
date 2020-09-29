c7d077729376e521ed1a04d79d787c7197af05a7
Landon <landon@pop-os.localdomain>
2020-09-29 14:06:33 -0600

    Updated config files for deployment

--- melissa/melissa/settings.py
+++ melissa/melissa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dno4_e52rw#lrmbm+n(pplgr=%%jvlmz7h)@h(j&b327)4q$pt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.73']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'melissa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/var/www/mel_site/static/'
